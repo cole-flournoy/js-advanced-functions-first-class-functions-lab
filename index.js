807684ccb7e6062c6e8b318726ccbdd721feed28
Cole Flournoy <flournoy.cole@gmail.com>
2021-04-25 14:31:57 -0400

    commit tree 2237bffb1c5f535d70084031209fce52cd7fb218

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+  return drivers.slice(0, 2)
+}
+
+const returnLastTwoDrivers = function(drivers) {
+  return drivers.slice(drivers.length - 2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+// function createFareMultiplier(mult, fare) {
+//   return mult * fare
+// }
+
+const createFareMultiplier = function(mult){
+  return function(fare){
+    return mult * fare
+  }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = function (drivers, driversFunc) {
+  return driversFunc(drivers)
+}
